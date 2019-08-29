589177f495f2ca14084a29e91ecc60dd0a725540
ZimingYuan <yzmyzm99@outlook.com>
2019-08-29 13:55:34 +0800

    Use <canvas> to resize the background image to fit the screen resolution and make the background image over all the elements to fix display bug.

--- Change_Webpages_Background.js
+++ Change_Webpages_Background.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Change webpages' background(改变网页的背景)
-// @version      1.0
+// @version      2.0
 // @description  Change webpages' background to the picture you choose. Press F2 to change the picture, F4 to change the opacity of the foreground elements.(根据你选择的图片更换网页的背景，按F2更换背景，按F4改变前景元素的透明度。)
 // @author       Jeremy Yuan QQ:1223962053
 // @require      http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
@@ -19,17 +19,18 @@
 
     let url = GM_getValue('url');
     let opacity = GM_getValue('opacity');
-    if (opacity == undefined) opacity = '0.9';
+    if (opacity == undefined) opacity = '0.8';
 
     function ChangeOpacity() {
-        $('body').children(':not(#GM_ValuebarContainer, #GM_Background, style, script, iframe, svg, #initial-loading, #isolated)').css('opacity', opacity);
+        $('body').children('#GM_Background').css('opacity', (1 - parseFloat(opacity)).toString());
     }
 
     function AddValuebar() {
-        GM_addStyle('#GM_ValuebarContainer { position: fixed; top: 40px; left: 45%; width: 15%; height: 20px; background-color: #66cccc; display: none; border: 0.5px solid; border-radius: 8px; z-index: 100; }');
+        GM_addStyle('#GM_ValuebarContainer { position: fixed; top: 40px; left: 45%; width: 15%; height: 20px; background-color: #66cccc; display: none; border: 0.5px solid; border-radius: 8px; z-index: 10000; }');
         GM_addStyle('#GM_Valuebar { -webkit-appearance: none; transform: translateX(5%); width: 91%; height: 100%; background-color: #0000; }');
         GM_addStyle('#GM_Valuebar::-webkit-slider-thumb { -webkit-appearance: none; width: 14px; height: 14px; transform: translateY(-3px); border-radius: 7px; border: 0.5px solid #000; background-color: #ff99cc; }');
         GM_addStyle('#GM_Valuebar::-webkit-slider-runnable-track { -webkit-appearance: none; height: 8px; border-radius: 4px; border: 0.5px solid #000; background-color: #ccff66; }');
+        GM_addStyle('#GM_Background { position: fixed; top: 0; left: 0; z-index: 9999; pointer-events:none; }');
         let valuebar = '<div id="GM_ValuebarContainer"><input type="range" id="GM_Valuebar" min="0.0" max="1.0" step="0.05" value="' + opacity + '"></div>';
         $('body').append(valuebar);
         $('#GM_Valuebar').change(() => {
@@ -45,11 +46,23 @@
         });
     }
 
+    function DrawBackground(url) {
+        let canvas = $('#GM_Background')[0];
+        canvas.width = screen.width;
+        canvas.height = screen.height;
+        let ctx = canvas.getContext('2d');
+        let img = new Image(); img.src = url;
+        img.onload = () => {
+            let scale = window.devicePixelRatio / 1.25;
+            ctx.scale(screen.width / img.width / scale, screen.height / img.height / scale);
+            ctx.drawImage(img, 0, 0);
+        };
+    }
+
     function AddBackground() {
-        $('body').css('opacity', '0.999');
-        GM_addStyle('#GM_Background { background-image: url(' + url + '); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: -10; background-repeat: no-repeat; background-size: cover; background-color: #fff; opacity: 0.6; }');
-        let background = '<div id="GM_Background"></div>';
+        let background = '<canvas id="GM_Background"></canvas>';
         $('body').append(background);
+        DrawBackground(url);
         ChangeOpacity(opacity);
         AddValuebar();
     }
@@ -64,7 +77,7 @@
             if (geturl != null) {
                 GM_setValue('url', geturl);
                 if (url == undefined) { url = geturl; AddBackground(); }
-                else { url = geturl; $('#GM_Background').css('background-image', 'url(' + url + ')'); }
+                else { url = geturl; DrawBackground(url); }
             }
         }
     });
--- README.md
+++ README.md
@@ -4,10 +4,12 @@ A grease monkey script to change webpages' background.
 ### Description
 Change webpages' background to the picture you choose through the picture's url. Press F2 to change the picture, F4 to change the opacity of the foreground elements.
 
+### Installation
+Create a new userscript in your grease monkey script manager and copy the text in 'Change_Webpages_Background.js' to it.
+
 ### Known Problems
 * Because of github's security policy, the background can't display on github.
-* Some long webpages such as the mobile version of sina weibo and jianshu(jianshu.com) can't display normally on Chrome(bug?) when the background displays. You can press F4 to hide the background or use other explorer such as Firefox or Edge.
 
 ### Pictures
-![Before](https://github.com/ZimingYuan/Change-webpages-background/blob/master/Before.png)
-![After](https://github.com/ZimingYuan/Change-webpages-background/blob/master/After.png)
+![Before](https://github.com/ZimingYuan/Change-webpages-background/blob/master/Before.jpg)
+![After](https://github.com/ZimingYuan/Change-webpages-background/blob/master/After.jpg)
